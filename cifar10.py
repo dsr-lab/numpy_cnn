Added HE weights initialization. Bug fixing in the train procedure that was wrongly referencing to test set rather than valid set. Temporarily removed the dataset normalization based on the mean and std of the dataset.

cifar10.py
@@ -3,7 +3,7 @@ import pickle
 import tarfile
 import urllib.request
 import numpy as np
-
+import torch
 
 class Cifar10:
 
@@ -83,7 +83,7 @@ class Cifar10:
                 file_images = dictionary.get(b'data')
                 file_labels = dictionary.get(b'labels')
 
-                #file_images = file_images / 255
+                file_images = file_images / 255
                 file_images = file_images.reshape(10000, 3, 32, 32)
 
                 images[idx * 10000:10000 * (idx + 1)] = file_images
@@ -93,6 +93,7 @@ class Cifar10:
         self.train_labels, self.test_labels = labels[:50000], labels[50000:]
 
         # Shuffle the train set
+        #np.random.seed(42)
         permutation_indices = np.random.permutation(len(self.train_images))
         self.train_images = self.train_images[permutation_indices]
         self.train_labels = self.train_labels[permutation_indices]
@@ -100,11 +101,11 @@ class Cifar10:
         # Normalize with mean and std
         mean_train = self.train_images.mean(axis=(0, 2, 3), keepdims=True)
         std_train = self.train_images.std(axis=(0, 2, 3), keepdims=True)
-        self.train_images = (self.train_images - mean_train) / std_train
+        self.train_images = (self.train_images - 0.5) / 0.5
 
         mean_test = self.test_images.mean(axis=(0, 2, 3), keepdims=True)
         std_test = self.test_images.std(axis=(0, 2, 3), keepdims=True)
-        self.test_images = (self.test_images - mean_test) / std_test
+        self.test_images = (self.test_images - 0.5) / 0.5
 
         # Create the validation set
         self.validation_images = self.train_images[45000:]
@@ -112,14 +113,6 @@ class Cifar10:
 
         self.train_images = self.train_images[:45000]
         self.train_labels = self.train_labels[:45000]
-
-        a = self.train_images.min()
-        b = self.train_images.max()
-
-        c = self.validation_images.min()
-        d = self.validation_images.max()
-
-        print()
 
     @staticmethod
     def __download_progress(block_num, block_size, total_size):

convolution.py
@@ -225,11 +225,17 @@ def generate_kernel(input_channels=3, output_channels=16, kernel_h=3, kernel_w=3
 
     if random:
         # return np.random.rand(output_channels, input_channels, kernel_h, kernel_w) * np.sqrt(1./3.)
-        n = input_channels
-        for k in (kernel_h, kernel_w):
-            n *= k
-        stdv = 1. / np.sqrt(n)
-        return np.random.uniform(low=-stdv, high=stdv, size=(output_channels, input_channels, kernel_h, kernel_w))
+        n = output_channels * kernel_h * kernel_w
+        #for k in (kernel_h, kernel_w):
+        #    n *= k
+
+        receptive_field_size = kernel_h * kernel_w
+        fan_in = input_channels * receptive_field_size
+
+        #stdv = 1. / np.sqrt(n)
+        #return np.random.uniform(low=-stdv, high=stdv, size=(output_channels, input_channels, kernel_h, kernel_w))
+
+        return np.random.standard_normal((output_channels, input_channels, kernel_h, kernel_w)) * np.sqrt(2 / fan_in)
 
     else:
         return np.ones((output_channels, input_channels, kernel_h, kernel_w)) * 2

train.py
@@ -15,12 +15,12 @@ DENSE_DROPOUT_PROBABILITY = 0.8
 
 OPTIMIZER = 'ADAM'  # Valid values: ADAM, MOMENTUM
 CONV_PADDING = 0
-TRAIN_SMALL_DATASET = False
+TRAIN_SMALL_DATASET = True
 
 
 def train_network(train_images, train_labels,
-                  test_images, test_labels,
-                  valid_images, valid_labels,
+                    valid_images, valid_labels,
+                    test_images, test_labels,
                   use_fast_conv,
                   use_dropout):
 
@@ -35,13 +35,21 @@ def train_network(train_images, train_labels,
     kernel = generate_kernel(input_channels=3, output_channels=8, kernel_h=3, kernel_w=3)
     kernel2 = generate_kernel(input_channels=8, output_channels=16, kernel_h=3, kernel_w=3)
 
-    fc1_stdv = 1. / np.sqrt(3136)
-    fc1_w = np.random.uniform(low=-fc1_stdv, high=fc1_stdv, size=(128, 3136))  # 16384 is the size after the maxpool
-    fc1_b = np.random.uniform(low=-fc1_stdv, high=fc1_stdv, size=(128, 1))
 
-    fc2_stdv = 1. / np.sqrt(64)
-    fc2_w = np.random.uniform(low=-fc2_stdv, high=fc2_stdv, size=(10, 128))
-    fc2_b = np.random.uniform(low=-fc2_stdv, high=fc2_stdv, size=(10, 1))
+
+    #fc1_stdv = 1. / np.sqrt(3136)
+    #fc1_w = np.random.uniform(low=-fc1_stdv, high=fc1_stdv, size=(128, 3136))  # 16384 is the size after the maxpool
+    # fc1_b = np.random.uniform(low=-fc1_stdv, high=fc1_stdv, size=(128, 1))
+    fc1_w = np.random.standard_normal((128, 3136)) * np.sqrt(2/3136)
+    fc1_b = np.zeros((128, 1)) * np.sqrt(2/3136)
+    #fc1_b = np.random.randn(128, 1) * np.sqrt(2/3136)
+
+    #fc2_stdv = 1. / np.sqrt(128)
+    #fc2_w = np.random.uniform(low=-fc2_stdv, high=fc2_stdv, size=(10, 128))
+    #fc2_b = np.random.uniform(low=-fc2_stdv, high=fc2_stdv, size=(10, 1))
+    fc2_w = np.random.standard_normal((10, 128)) * np.sqrt(2/128)
+    fc2_b = np.zeros((10, 1)) * np.sqrt(2 / 128)
+    #fc2_b = np.random.randn(10, 1) * np.sqrt(2/128)
 
     learning_rate = 1e-3
 