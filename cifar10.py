Fixed a bug in the fast maxpooling backprop operation.

cifar10.py
@@ -83,8 +83,8 @@ class Cifar10:
                 file_images = dictionary.get(b'data')
                 file_labels = dictionary.get(b'labels')
 
-                file_images = file_images / 255
-                file_images = (file_images - 0.5) / 0.5
+                #file_images = file_images / 255
+                #file_images = (file_images - 0.5) / 0.5
                 file_images = file_images.reshape(10000, 3, 32, 32)
 
                 images[idx * 10000:10000 * (idx + 1)] = file_images
@@ -98,24 +98,22 @@ class Cifar10:
         permutation_indices = np.random.permutation(len(self.train_images))
         self.train_images = self.train_images[permutation_indices]
         self.train_labels = self.train_labels[permutation_indices]
-        a = self.train_labels.shape
-        print()
 
         # Normalize with mean and std
-        # mean_train = self.train_images.mean(axis=(0, 2, 3), keepdims=True)
-        # std_train = self.train_images.std(axis=(0, 2, 3), keepdims=True)
-        # self.train_images = (self.train_images - mean_train) / std_train
+        mean_train = self.train_images.mean(axis=(0, 2, 3), keepdims=True)
+        std_train = self.train_images.std(axis=(0, 2, 3), keepdims=True)
+        self.train_images = (self.train_images - mean_train) / std_train
 
-        # mean_test = self.test_images.mean(axis=(0, 2, 3), keepdims=True)
-        # std_test = self.test_images.std(axis=(0, 2, 3), keepdims=True)
-        #self.test_images = (self.test_images - 0.5) / 0.5
+        mean_test = self.test_images.mean(axis=(0, 2, 3), keepdims=True)
+        std_test = self.test_images.std(axis=(0, 2, 3), keepdims=True)
+        self.test_images = (self.test_images - 0.5) / 0.5
 
         # Create the validation set
-        self.validation_images = self.train_images[45000:]
-        self.validation_labels = self.train_labels[45000:]
+        self.validation_images = self.train_images[49000:]
+        self.validation_labels = self.train_labels[49000:]
 
-        self.train_images = self.train_images[:45000]
-        self.train_labels = self.train_labels[:45000]
+        self.train_images = self.train_images[:49000]
+        self.train_labels = self.train_labels[:49000]
 
     @staticmethod
     def __download_progress(block_num, block_size, total_size):

cross_entropy.py
@@ -5,7 +5,7 @@ import torch
 def cross_entropy(scores, targets, t):
 
     # TODO: could be useful to clip prediction values.
-    #scores = np.clip(scores, 1e-8, 1. - 1e-8)
+    scores = np.clip(scores, 1e-8, 1. - 1e-8)
 
     n_predictions = scores.shape[1]
     ce = -np.sum(targets*np.log(scores+1e-8))/n_predictions

max_pooling.py
@@ -190,7 +190,11 @@ def fast_maxpool_backprop(gradient_values, conv_shape, padding, stride, max_pool
     #      [[21, 22], [23, 24]]]
     # ]
     # bp = np.asarray(bp, dtype=np.float64)
-    bp_flattened = gradient_values.reshape(n_channels, -1)
+
+    bp_flattened = gradient_values.reshape(gradient_values.shape[0] * gradient_values.shape[1],
+                                   gradient_values.shape[2] * gradient_values.shape[3])
+    bp_flattened = np.array(np.vsplit(bp_flattened, conv_shape[0]))
+    bp_flattened = np.concatenate(bp_flattened, axis=-1)
 
     # the convolution shape expected is then (1,3,4,4)
     # delta_conv = np.zeros((2, 3, 4, 4))  # shape of the gradient
@@ -200,7 +204,7 @@ def fast_maxpool_backprop(gradient_values, conv_shape, padding, stride, max_pool
 
     # Those are indexes channel wise    n_channels = 3
     # pos_result = [[2, 3, 0, 1, 2, 3, 0, 1], [2, 0, 2, 2, 2, 0, 2, 2], [2, 0, 0, 1, 2, 0, 0, 3]]
-    # pos_result = np.asarray(pos_result)
+    # pos_result = np.asarray(pos_result)delta_conv_col
     # pos_result_shape = pos_result.shape
 
     row_coefficient = delta_conv_col.shape[0] // n_channels
@@ -215,13 +219,18 @@ def fast_maxpool_backprop(gradient_values, conv_shape, padding, stride, max_pool
 
     np.add.at(delta_conv_col, (pos_result, col_indices), bp_flattened)
 
-    # delta_conv = delta_conv_col.reshape(2, 3, 4, 4)
-    # delta_conv = delta_conv_col.reshape(conv_shape)
-
+    #a = np.array(np.hsplit(delta_conv_col, conv_shape[0]))
+    #b = a.reshape(2,2,4,4)
+    #delta_conv_col = delta_conv_col
     delta_conv = col2im(delta_conv_col, conv_shape, max_pool_size, max_pool_size, stride, padding)
-    # n_images = conv_shape[0]
-    # delta_conv = delta_conv_col.reshape(n_images, -1)
-    # delta_conv = np.array(np.hsplit(delta_conv, n_channels))
-    # delta_conv = delta_conv.reshape(conv_shape)
+    '''
+    delta_conv1 = delta_conv.reshape(2, -1)
+    delta_conv2 = np.array(np.hsplit(delta_conv1, conv_shape[1]))
+    #delta_conv = delta_conv.reshape(2, 2, 4, 4)
+
+    delta_conv2 = delta_conv2.reshape(2,2,4,4)
+    '''
+
+
 
     return delta_conv

softmax.py
@@ -5,8 +5,17 @@ import torch.nn.functional as F
 def softmax(scores):
     #scores = np.asarray(scores, dtype=np.float64)
 
-    e_x = np.exp(scores - np.max(scores))
+    #np.exp(scores) / np.sum(np.exp(scores), axis=1, keepdims=True)
+
+    e_x = np.exp(scores) # - np.max(scores))
     result = e_x / e_x.sum(axis=0)
+
+    # OLD VERSION
+    # e_x2 = np.exp(scores - np.max(scores))
+    # result2 = e_x2 / e_x2.sum(axis=0)
+
+
+
     # test = np.sum(result[:, 2])
 
     # maxes = torch.mean(torch.as_tensor(scores), 1, keepdim=True)[0]

train.py
@@ -13,18 +13,19 @@ BATCH_SIZE = 128
 EPOCHS = 50
 CONV_DROPOUT_PROBABILITY = 0.9
 DENSE_DROPOUT_PROBABILITY = 0.8
+CONV_PADDING = 0
 
 OPTIMIZER = 'ADAM'  # Valid values: ADAM, MOMENTUM
-CONV_PADDING = 0
+
 TRAIN_SMALL_DATASET = False
+USE_CIFAR_10 = True
 
 
 def train_network(train_images, train_labels,
-                    valid_images, valid_labels,
-                    test_images, test_labels,
+                  valid_images, valid_labels,
+                  test_images, test_labels,
                   use_fast_conv,
                   use_dropout):
-
     # TODO: optimize this without creating a copy of the dataset
     train_images_batches = np.split(train_images, np.arange(BATCH_SIZE, len(train_images), BATCH_SIZE))
     train_images_labels = np.split(train_labels, np.arange(BATCH_SIZE, len(train_labels), BATCH_SIZE))
@@ -32,14 +33,24 @@ def train_network(train_images, train_labels,
     val_images_batches = np.split(valid_images, np.arange(BATCH_SIZE, len(valid_images), BATCH_SIZE))
     val_images_labels = np.split(valid_labels, np.arange(BATCH_SIZE, len(valid_labels), BATCH_SIZE))
 
-    # Kernel for the convolution layer
-    kernel = generate_kernel(input_channels=1, output_channels=8, kernel_h=3, kernel_w=3)
-    kernel2 = generate_kernel(input_channels=8, output_channels=16, kernel_h=3, kernel_w=3)
+    input_channels = 1
+    fan_in = 2304
+    if USE_CIFAR_10:
+        input_channels = 3
+        fan_in = 3136
 
-    fc1_w = np.random.standard_normal((64, 2304)) * np.sqrt(2/2304)
+    # Kernel for the convolution layer
+    # kernel = generate_kernel(input_channels=input_channels, output_channels=8, kernel_h=3, kernel_w=3)
+    kernel = np.random.randn(8, 3, 3, 3) / np.sqrt(8192 / 2)
+    # kernel2 = generate_kernel(input_channels=8, output_channels=16, kernel_h=3, kernel_w=3)
+    kernel2 = np.random.randn(16, 8, 3, 3) / np.sqrt(7200 / 2)
+
+    # fc1_w = np.random.standard_normal((64, fan_in)) * np.sqrt(2 / fan_in)
+    fc1_w = np.random.randn(64, fan_in) / np.sqrt(fan_in / 2)
     fc1_b = np.zeros((64, 1))
 
-    fc2_w = np.random.standard_normal((10, 64)) * np.sqrt(2/64)
+    # fc2_w = np.random.standard_normal((10, 64)) * np.sqrt(2 / 64)
+    fc2_w = np.random.randn(10, 64) / np.sqrt(64 / 2)
     fc2_b = np.zeros((10, 1))
 
     learning_rate = 1e-3
@@ -94,8 +105,8 @@ def train_network(train_images, train_labels,
             else:
                 x_conv = convolve_2d(input_data, kernel, padding=CONV_PADDING)
 
+            a = x_conv.shape
             # Save the shape for later use (used during the backpropagation)
-            conv_out_shape = x_conv.shape
 
             if use_dropout:
                 x_conv = cnn_dropout(x_conv, CONV_DROPOUT_PROBABILITY)
@@ -157,7 +168,8 @@ def train_network(train_images, train_labels,
             # ################################################################################
             # BACKWARD PASS
             # ################################################################################
-            delta_2 = (scores - one_hot_encoding_labels)
+
+            delta_2 = (scores - one_hot_encoding_labels) / BATCH_SIZE  # TODO: check this
             d_fc2_w = delta_2 @ fc2_input.T
             d_fc2_b = np.sum(delta_2, axis=1, keepdims=True)
 
@@ -197,6 +209,16 @@ def train_network(train_images, train_labels,
                 conv1_delta, _ = fast_convolution_backprop(input_data, kernel, dX2, padding=CONV_PADDING)
             else:
                 conv1_delta, _ = convolution_backprop(input_data, kernel, dX2, padding=CONV_PADDING)
+
+            # Average the gradients
+            # a = d_fc1_w.shape
+            # d_fc1_w = np.average(d_fc1_w, axis=0)
+            # b = d_fc1_w.shape
+            # d_fc2_w = np.average(d_fc2_w, axis=0)
+            # d_fc1_b = np.average(d_fc1_b, axis=0)
+            # d_fc2_b = np.average(d_fc2_b, axis=0)
+            # conv1_delta = np.average(conv1_delta, axis=0)
+            # conv2_delta = np.average(conv2_delta, axis=0)
 
             if OPTIMIZER == 'ADAM':
                 momentum_w1 = beta1 * momentum_w1 + ((1 - beta1) * d_fc1_w)
@@ -347,26 +369,63 @@ def train_network(train_images, train_labels,
 
 
 def main():
-    dataset = Mnist()
+    if USE_CIFAR_10:
+        dataset = Cifar10()
+    else:
+        dataset = Mnist()
 
     train_images, train_labels, \
-        validation_images, validation_labels, \
-        test_images, test_labels = dataset.get_small_datasets() if TRAIN_SMALL_DATASET else dataset.get_datasets()
+    validation_images, validation_labels, \
+    test_images, test_labels = dataset.get_small_datasets() if TRAIN_SMALL_DATASET else dataset.get_datasets()
 
-    #a = np.array([0, 3, 0, 1, 0, 1, 2, 1, 0, 0, 0, 0, 1, 3, 4])
+    # a = np.array([0, 3, 0, 1, 0, 1, 2, 1, 0, 0, 0, 0, 1, 3, 4])
     unique, counts = np.unique(train_labels, return_counts=True)
     a = dict(zip(unique, counts))
     unique, counts = np.unique(validation_labels, return_counts=True)
     b = dict(zip(unique, counts))
     unique, counts = np.unique(test_labels, return_counts=True)
     c = dict(zip(unique, counts))
-
+    #
     train_network(train_images, train_labels,
                   validation_images, validation_labels,
                   test_images, test_labels,
                   True, False)
 
-    #convolution_method_comparisons()
+    # test_naive_fast_convolutions()
+
+    # n_images = 5
+    # X = dataset.train_images[:n_images]
+    # #X = np.random.randint(-100, 100, size=(2, 3, 15, 30))
+    # kernel = generate_kernel(input_channels=3, output_channels=8, kernel_h=3, kernel_w=3)
+    # kernel = np.random.randint(100, size=(8, 3, 3, 3))
+    # #
+    # # relu_input = np.random.rand(n_images, 8, 30, 30)
+    # gradient_values = np.random.rand(n_images, 8, 30, 30)
+    # gradient_values = np.random.randint(100, size=(2, 2, 3, 3))
+    # a = gradient_values.shape
+    #
+    # conv_data = [
+    #     [[[3, 1, 7, 2], [5, 1, 0, 9], [8, 2, 4, 9], [4, 3, 1, 1]],
+    #      [[12, 18, 0, 2], [33, 2, 55, 60], [5, 2, 4, 8], [4, 32, 101, 1]],
+    #      [[19, 1, 27, 2], [51, 1, 0, 9], [82, 2, 4, 9], [4, 3, 1, 11]]],
+    #     [[[3, 1, 7, 2], [5, 1, 0, 9], [8, 2, 4, 9], [4, 3, 1, 1]],
+    #      [[12, 18, 0, 2], [33, 2, 55, 60], [5, 2, 4, 8], [4, 32, 101, 1]],
+    #      [[19, 1, 27, 2], [51, 1, 0, 9], [82, 2, 4, 9], [4, 3, 1, 11]]]
+    # ]
+    # conv_data = [
+    #     [
+    #         [[155, 2, 3, 4], [5, 6, 7, 8], [9, 10, 31, 12], [13, 14, 44, 16]],
+    #         [[22, 799, 19, 20], [21, 27, 23, 24], [25, 26, 27, 28], [29, 30, 31, 57]]
+    #      ],
+    #     [
+    #         [[1, 2, 32, 49], [5, 6, 7, 48], [9, 10, 11, 12], [13, 14, 15, 22]],
+    #         [[172, 18, 19, 20], [21, 33, 23, 24], [55, 26, 27, 28], [29, 44, 31, 32]]
+    #     ]
+    # ]
+    # conv_data = np.asarray(conv_data)
+    # #
+    # # convolution_method_comparisons(X, kernel, gradient_values)
+    # test_naive_fast_max_pool(X)
 
     print()
 